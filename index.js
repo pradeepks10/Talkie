6e662fcf2b63274ef7ab8f580f4391db57770197
Pradeep Kr. Singh <pradeep940101@gmail.com>
2023-07-02 21:23:25 +0530

    modify_vercel.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
+const path = require('path');
 
 const PORT = process.env.PORT || 3000
 app.use(express.static(__dirname + '/public'))
-
+// app.use('/public', express.static(path.join(__dirname, 'public')));
 http.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
